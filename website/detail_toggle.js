7ab4793330d2ba98b499296b7c5c4eaa38de6ade
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-04-23 16:23:05 +0100

    change hiding to js for nojs fallback, fix loading race condition

--- website/detail_toggle.js
+++ website/detail_toggle.js
@@ -1,12 +1,17 @@
-const giftDetails = document.querySelector('.martini-gift__gift-details');
-const toggleLink = document.querySelector('.martini-gift__toggle-details');
-
-const elementIsVisible = el => el && el.style && el.style.display === 'block'; 
+const elementIsVisible = el => el && el.style && el.style.display !== 'none'; 
 
 const hideElement = el => el.style.display = 'none';
 
-const showElement = el => el.style.display = 'block';
+const showElement = el => el.style.removeProperty('display');
 
-const toggleVisibility = () => elementIsVisible(giftDetails) ? hideElement(giftDetails) : showElement(giftDetails);
+const toggleVisibility = el => elementIsVisible(el) ? hideElement(el) : showElement(el);
 
-toggleLink.addEventListener('click',toggleVisibility);
+const initialise = () => {
+    const giftDetails = document.querySelector('.martini-gift__gift-details');
+    const toggleLink = document.querySelector('.martini-gift__toggle-details');
+
+    hideElement(giftDetails);
+    toggleLink.addEventListener('click', () => toggleVisibility(giftDetails));
+};
+
+document.addEventListener('DOMContentLoaded', initialise);
