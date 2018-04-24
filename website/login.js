08bedf04f053890de3d885db8f770572d98202d6
FabrizioMontanari <joejimmybill@hotmail.com>
2018-04-24 14:29:38 +0200

    refactor: moved scripts to files
    ui changes

---
+++ website/login.js
@@ -0,0 +1,75 @@
+/*login dei poveri*/
+function checkLogin(username,password){
+	if (username === "nomeutente" && password==="password"){
+		console.log("credenziali riconosciute");
+		return true;
+	}
+	console.log("credenziali non riconosciute");
+	return false;
+}
+
+function loginSuccessUptadeUI(){
+	$(".login-content").addClass("hide");
+	$(".site-content").removeClass("hide");
+}
+
+function onAccediClick(){
+	var u=$("#username").val();
+	var p=$("#password").val();
+	if (checkLogin(u,p)){
+		setCookie("login_done","t");
+		loginSuccessUptadeUI();
+	}
+}
+
+function landingLoginCheck(){
+	var credential = getUrlVars();
+	console.log("found this cookie: " +getCookie("login_done"));
+	console.log("check cookie: " +getCookie("login_done")!=="t");
+	if ( !(credential.hasOwnProperty('password') && credential.hasOwnProperty('username')) && getCookie("login_done")!=="t" ) {
+		return;
+	}
+	else{
+		if (checkLogin(credential["username"],credential["password"]) || getCookie("login_done")==="t"){
+			setCookie("login_done","t");
+			loginSuccessUptadeUI();
+		}
+	}
+}
+// Read a page's GET URL variables and return them as an associative array.
+function getUrlVars(){
+	var vars = [], hash;
+	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+	for(var i = 0; i < hashes.length; i++){
+		hash = hashes[i].split('=');
+		vars.push(hash[0]);
+		vars[hash[0]] = hash[1];
+	}
+	return vars;
+}
+
+function setCookie(cname, cvalue) {
+	var ctmp = cname + "="+ cvalue;
+	document.cookie = ctmp;
+	console.log("cookie set: "+ ctmp);
+}
+
+function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+
+$(document).ready(function () {
+	landingLoginCheck();
+});
---
+++ website/map.js
@@ -0,0 +1,25 @@
+var map_text_data={
+"1":["Partenza","Qui arriviamo in aereo."],
+"2":["Prima tappa","Qui ci andiamo subito perché c'è il miglior ramen."],
+"3":["Seconda tappa","Qua facciamo una breve sosta alle terme."],
+"4":["Terza tappa","Qui c'è la statua gigante di Super Mario."],
+"5":["Arrivo","Questo giro panoramico ci fa vistare tutti i villaggini."],
+}
+
+function createImgUrl(step){
+	return 'https://raw.githubusercontent.com/FabrizioMontanari/caterel/master/0'+step+'.png';
+}
+function MapChange(btn){
+	var step = $(btn).data("step");
+	$("#map-map").css('background-image', 'url('+createImgUrl(step)+')');
+	$("#map-title").text(map_text_data[step][0]);
+	$("#map-txt").text(map_text_data[step][1]);
+}
+
+
+$.fn.preload = function() {
+	this.each(function(){
+		$('<img/>')[0].src = this;
+	});
+}
+$([createImgUrl(1),createImgUrl(2),createImgUrl(3),createImgUrl(4),createImgUrl(5)]).preload();
