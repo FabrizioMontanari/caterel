41e9f79c1d6dbdf0e073a5a130192957c09e3c35
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-05-04 00:44:36 +0100

    es6 (almost) all the things

--- edgeLambda/auth.js
+++ edgeLambda/auth.js
@@ -1,25 +1,18 @@
-export function handler(event, context, callback) {
-	// Get request and request headers
-	const request = event.Records[0].cf.request;
-	const headers = request.headers;
-	// Configure authentication
-	const authUser = 'martini';
-	const authPass = 'sposi';
-	// Construct the Basic Auth string
-	const authString = 'Basic ' + new Buffer(authUser + ':' + authPass).toString('base64');
-	// Require Basic authentication
-	if (typeof headers.authorization == 'undefined' || headers.authorization[0].value != authString) {
-		const body = 'Unauthorized';
-		const response = {
-			status: '401',
-			statusDescription: 'Unauthorized',
-			body: body,
-			headers: {
-				'www-authenticate': [{ key: 'WWW-Authenticate', value: 'Basic' }]
-			},
-		};
-		callback(null, response);
-	}
-	// Continue request processing if authentication passed
-	callback(null, request);
+const unauthorizedResponse = {
+    status: '401',
+    statusDescription: 'Unauthorized',
+    body: 'Unauthorized',
+    headers: { 'www-authenticate': [{ key: 'WWW-Authenticate', value: 'Basic' }] }
+};
+
+const expectedAuthString = `Basic ${new Buffer('martini:sposi').toString('base64')}`;
+
+const isAuthValid = auth => auth instanceof Array && auth[0].value === expectedAuthString;
+
+exports.handler = (event, context, callback) => {
+    const { request } = event.Records[0].cf;
+    
+    const callbackValue = isAuthValid(request.headers.authorization) ? request : unauthorizedResponse;
+
+    callback(null, callbackValue);
 }
