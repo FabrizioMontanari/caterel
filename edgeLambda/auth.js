03c6e2e041cb79aa2e4bc1e79fc81e084551d017
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-05-16 23:16:47 +0100

    add login stub

--- edgeLambda/auth.js
+++ edgeLambda/auth.js
@@ -2,15 +2,29 @@ const isAuthorized = cookie => cookie && cookie.length && cookie[0].value && coo
 
 const sanitiseHttpMethod = request => request.method = 'GET';
 
+const passwordIsValid = pass => true;
+
+const handleLogin = request => {
+    if (request.uri === '/login.html' && request.method === 'POST' && passwordIsValid(request.passwordLocation)) {
+        request.headers.cookie[0] = {
+            value: 'Logged=1;',
+            key: 'Cookie'
+        };
+        request.uri = '/';
+    }
+}
+
 exports.handler = (event, context, callback) => {
     const { request } = event.Records[0].cf;
     const { uri, headers } = request;
     const cookie = headers.cookie;
 
-    if(!isAuthorized(cookie)) {
+    handleLogin(request);
+    sanitiseHttpMethod(request);
+
+    if (!isAuthorized(cookie)) {
         request.uri = '/login.html';
     }
-
-    sanitiseHttpMethod(request);
+    
     callback(null, request);
 }
