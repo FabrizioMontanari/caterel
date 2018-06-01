e0666deac545b257121e5ec43d4ca3d93be61e3c
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-06-01 19:35:38 +0100

    add secret login logic

--- backend/martini.py
+++ backend/martini.py
@@ -2,8 +2,17 @@ from flask import *  # lazy.
 app = Flask(__name__)
 
 
+BASE_URL = 'test.imartinisisposano.it'
+
+
 @app.route('/')
 def index():
+    print(request.host)
+    if request.host == f'km.{BASE_URL}':
+        res = make_response(redirect(f'http://{BASE_URL}', 302))
+        res.set_cookie('is_logged_in', 'True', domain=f'{BASE_URL}')
+        return res
+
     is_logged_in = request.cookies.get('is_logged_in')
     return f'Hello, World! {is_logged_in}'
 
