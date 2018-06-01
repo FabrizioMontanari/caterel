66423df5f6ecb3df81c241ec421cbbbb059b7b62
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-06-01 18:35:44 +0100

    add login/logout example

--- backend/martini.py
+++ backend/martini.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import *  # lazy.
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    is_logged_in = request.cookies.get('is_logged_in')
+    return f'Hello, World! {is_logged_in}'
+
+
+@app.route('/login')
+def login():
+    res = make_response('logged in!')
+    res.set_cookie('is_logged_in', 'True')
+    return res
+
+
+@app.route('/logout')
+def logout():
+    res = make_response('logged out!')
+    res.set_cookie('is_logged_in', expires=0)
+    return res
