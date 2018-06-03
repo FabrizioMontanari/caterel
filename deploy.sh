43523925af0dc1e1b107988c783accaba445819d
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-06-03 15:25:28 +0100

    tweak deploy to work with new cloudfront setup

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,15 @@ source env/bin/activate
 python build.py
 
 # copy static files to s3
-printf 'Synchronizing files to s3...'
-aws s3 sync static s3://static.imartinisisposano.it --acl public-read > /dev/null
-printf 'Done\n'
+echo 'Synchronizing files to s3...'
+aws s3 sync static s3://www.imartinisisposano.it/static
+echo 'Done'
 
 # deploy with zappa to lambda
-printf 'Deploying backend to lambda...'
+echo 'Deploying backend to lambda...'
 cd backend
 zappa update ${1:-dev}
-printf 'Done\n'
+echo 'Done'
 
 # clean up
 cd - > /dev/null
