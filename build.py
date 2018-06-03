aee2b434ca84aa2f68ce8c0d23476e026d659522
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-06-03 13:33:43 +0100

    rename deploy to build, recover zappa settings

--- build.py
+++ build.py
@@ -4,22 +4,25 @@ import shutil
 
 
 # clean up
+print('Cleaning up destination folders...')
 shutil.rmtree('static', ignore_errors=True)
 shutil.rmtree('backend', ignore_errors=True)
 
-
-# css
+# css compile
+print('Compiling css...')
 pathlib.Path('static/css').mkdir(parents=True, exist_ok=True) 
 sass.compile(dirname=('src/sass', 'static/css'), output_style='compressed')
 
-
-# static files
+# static files copy
+print('Copying static files...')
 shutil.copytree('src/fonts', 'static/fonts')
 shutil.copytree('src/img', 'static/img')
 shutil.copytree('src/js', 'static/js')
 
-
-# backend files
+# backend files copy
+print('Copying bakend code...')
 shutil.copytree('src/templates', 'backend/templates')
 shutil.copy2('src/martini.py', 'backend')
 shutil.copy2('src/zappa_settings.json', 'backend')
+
+print('Build step completed.\n')
