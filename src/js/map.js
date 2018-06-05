87c3a8d748d8527743774ea0752a52849bac9045
FabrizioMontanari <ffabrizio.montanari@gmail.com>
2018-06-05 12:31:04 -0700

    WIP map

--- src/js/map.js
+++ src/js/map.js
@@ -6,12 +6,38 @@ var map_text_data={
 "5":["Arrivo","Questo giro panoramico ci fa vistare tutti i villaggini."],
 }
 
+var map_image_map = {
+	"1":"0",
+	"2":"1",
+	"3":"2-3",
+	"4":"2-3",
+	"5":"4-5-6-7",
+	"6":"4-5-6-7",
+	"7":"4-5-6-7",
+	"8":"8",
+	"9":"9",
+	"10":"10-11",
+	"11":"10-11",
+	"12":"12-13-14-15-16-17-18",
+	"13":"12-13-14-15-16-17-18",
+	"14":"12-13-14-15-16-17-18",
+	"15":"12-13-14-15-16-17-18",
+	"16":"12-13-14-15-16-17-18",
+	"17":"12-13-14-15-16-17-18",
+	"18":"12-13-14-15-16-17-18",
+	"19":"12-13-14-15-16-17-18",
+	"20":"18",
+}
+
 function createImgUrl(step){
-	return '//static.imartinisisposano.it/img/map/'+step+'.jpg';
+	if(window.location.href.indexOf('127.0.0.1')>=0)
+		return '//127.0.0.1:8000/static/img/map/JMap-Day'+map_image_map[step]+'.png';
+	else
+		return '//static.imartinisisposano.it/img/map/JMap-Day'+map_image_map[step]+'.png';
 }
 function MapChange(btn){
 	var step = $(btn).data("step");
-	if ( step !=1 && $("#martini-gift__map__map").css('background-image').toLowerCase().indexOf("1.jpg") !== -1){
+	if ( step !=1 && $("#martini-gift__map__map").css('background-image').indexOf("JMap-Day0.png") !== -1){
 		$("#martini-gift__map__map").addClass("zoomin");
 		$("#martini-gift__map__map").removeClass("zoomout");
 		setTimeout(function(){
@@ -38,7 +64,12 @@ $.fn.preload = function() {
 }
 
 $(document).ready(function () {
-	$([createImgUrl(1),createImgUrl(2),createImgUrl(3),createImgUrl(4),createImgUrl(5)]).preload();
+	url_list=[];
+	for (var i = 0; i < Object.keys(map_image_map).length; i++){
+		url_list.push(createImgUrl(i+1));
+	}
+	
+	$(url_list).preload();
 	setTimeout(function(){
 							$(".martini-preloader").addClass("hide");
 						}, 500);
