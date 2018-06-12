dd46c42ade59c9c22d66aa9395a4905b40d5f7a1
FabrizioMontanari <ffabrizio.montanari@gmail.com>
2018-06-12 13:55:46 -0700

    WIP managing secret_key

--- build.py
+++ build.py
@@ -33,5 +33,6 @@ verbose_log('    Copying bakend code...')
 shutil.copytree('src/templates', 'backend/templates')
 shutil.copy2('src/martini.py', 'backend')
 shutil.copy2('src/zappa_settings.json', 'backend')
+shutil.copy2('src/secret_key.json', 'backend')
 
 print('Done')
