beaeb2af857422915324110487e39399767dba0b
FabrizioMontanari <ffabrizio.montanari@gmail.com>
2018-06-12 05:32:04 -0700

    first implementation of confirmation, both server- and client-side

--- src/js/confirmation.js
+++ src/js/confirmation.js
@@ -1,6 +1,6 @@
 /*utilities*/
 function escape(txt){
-	return $("<div>").text(txt).html();
+	return $('<div>').text(txt).html();
 }
 function toTitleCase(str) {
 	return str.replace(/\w\S*/g, function(txt){
@@ -12,15 +12,15 @@ function toTitleCase(str) {
 /**
  * Preparo il contenuto e mostro l'overlay
  */
-function onConfirmClick(){
+function onCiSaroClick(){
 	fillConfirmation();
-	$("#confirm-overlay").removeClass("hide");
+	$('#confirm-overlay').removeClass('hide');
 }
 /**
  * Costruisco tutti gli elementi dell'overlay leggendo i campi compilati dall'utente
  */
 function fillConfirmation(){
-	var overlay = $("#confirm-overlay");
+	var overlay = $('#confirm-overlay');
 	overlay.empty();
 	var overlay_content = '<h1 class="martini-confirm__container__overlay__title">confermi la presenza di</h1>\n';
 	overlay_content += '<p class="martini-confirm__container__overlay__member">'+toTitleCase(escape($('input[name=main_nome]').val()))+' '+toTitleCase(escape($('input[name=main_cognome]').val()))+', menu '+$('select[name=main_menu]').val()+'</p>\n';
@@ -32,16 +32,17 @@ function fillConfirmation(){
 		}
 	});
 		
-	overlay_content += '<div class="martini-confirm__container__overlay__submit">CONFERMA</div>\
-						<div class="martini-confirm__container__overlay__back" onclick="onBackClick()">MODIFICA</div>';
+	overlay_content += '<div class="martini-confirm__container__overlay__submit" onclick="onConfermaClick();">CONFERMA</div>\
+						<div class="martini-confirm__container__overlay__back" onclick="onBackClick();">MODIFICA</div>';
 	overlay.append(overlay_content);
 	return
 }
 /**
  * Riempe la sezione famiglia.
  * acceta una array di nomi dei familiari es: ["Marco Baldo", "Bella Lui", "Topo Gigio"]
+ * In caso di +1 viene aggiunto un solo membro e si inserisce un inputfield hidden
  */
-function fillFamily(family){
+function fillFamily(family, is_plusone){
 	if(!Array.isArray(family)){
 		console.log("Errore: l'argomento non è un array");
 		return;
@@ -49,9 +50,13 @@ function fillFamily(family){
 	var container = $('#martini-confirm__container__family');
 	container.empty();
 	family.forEach(function(member, i){
+		var name_input=!is_plusone?	'<input type="text" name="familiare_'+i+'_nome" value="'+member+'" readonly>':
+									'<input type="text" name="familiare_'+i+'_nome" value="" placeholder="Nome e Cognome">\
+									<input type="hidden" name="plus_one" value="plus_one_'+i+'">'
+
 		var new_element = '<div id="familiare_'+i+'" class="martini-confirm__container__family__member col-sm-5">\
 					<div class="martini-confirm__container__family__member__name">\
-						<input type="text" name="familiare_'+i+'_nome" value="'+member+'" readonly>\
+						'+name_input+'\
 						<div id="familiare_'+i+'_confirmed" class="checkbox" onclick="$(this).toggleClass(\'checked\')">&nbsp;</div>\
 					</div>\
 					<p>menu</p>\
@@ -69,5 +74,125 @@ function fillFamily(family){
 }
 
 function onBackClick(){
-	$("#confirm-overlay").addClass("hide");
+	$('#confirm-overlay').addClass('hide');
+}
+
+/**
+ * invia la richiesta per verificare la presenza dell'invitato nella lista e gestisce la risposta.
+ * Se l'utente esiste si mostrano le opzioni da compilare e gli eventuali familiari
+ */
+function onUserNomeCognomeEntered(){
+	var nome = escape($('input[name=main_nome]').val().toLowerCase());
+	var cognome = escape($('input[name=main_cognome]').val().toLowerCase());
+	//prevent request when empty
+	if(nome === '' || cognome === ''){
+		return;
+	}
+	//prevent request when already checked
+	if($('#checkbox_main').hasClass('checked') || $('#checkbox_main').hasClass('checking')){
+		return;
+	}
+	$('#checkbox_main').addClass('checking');
+	$.ajax({
+		type: 'POST',
+		url: '/get_family',
+		/*beforeSend: function(xhr){},*/
+		cache: false,
+		data: JSON.stringify({
+			'target': nome + ' ' + cognome,
+		}),
+		contentType: 'application/json; charset=utf-8',
+		// crossDomain: false,
+		dataType: 'json'
+	}).done(function(data) {
+		console.log('OK; data=%o', data);
+		if (data.family===null){
+			//remove checking status
+			$('#checkbox_main').removeClass('checking');
+
+			$('#not_invited').removeClass('hide');
+			$('#select_your_menu_text').addClass('hide');
+			$('#select_main_menu').addClass('hide');
+			$('#conferma_cari_text').addClass('hide');
+			$('#martini-confirm__container__family').addClass('hide');
+			$('#notes').addClass('hide');
+			return;
+		}
+		//lock curent user
+		$('input[name=main_nome]').prop('readonly',true);
+		$('input[name=main_cognome]').prop('readonly',true);
+		//remove checking status
+		$('#checkbox_main').removeClass('checking');
+		//check flag
+		$('#checkbox_main').addClass('checked');
+		
+		//show fields
+		$('#not_invited').addClass('hide');
+		$('#select_your_menu_text').removeClass('hide');
+		$('#select_main_menu').removeClass('hide');
+		$('#conferma_cari_text').removeClass('hide');
+		$('#martini-confirm__container__family').removeClass('hide');
+		$('#notes').removeClass('hide');
+		fillFamily(data.family, data.is_plusone);
+		return;
+	}).fail(function(jqXHR, textStatus) {
+		console.log('FAILURE: %o', jqXHR);
+		console.log('FAILURE: %o', textStatus);
+	});
+}
+
+
+/**
+ * invia i dati della prenotazione
+ */
+function onConfermaClick(){
+	main_nome = escape($('input[name=main_nome]').val()) + ' ' + escape($('input[name=main_cognome]').val());
+	main_menu = $('select[name=main_menu]').val();
+	main_note = $('textarea[name=note]').val();
+	family = [{'nome':main_nome,'menu':main_menu}];
+
+	$('div[id*="familiare_"][id$="_confirmed"]').each(function(i){
+		if ($( this ).hasClass('checked')){
+			member = {
+					'nome':escape($('input[name=familiare_'+i+'_nome]').val()), 
+					'menu':$('select[name=familiare_'+i+'_menu]').val(),
+				};
+			is_plus_one = $('input[name=plus_one][type=hidden]').length == 1;
+			if(is_plus_one){
+				member['is_plusone_of'] = main_nome;
+			}
+			family.push(member);
+		}
+	});
+
+
+	var dati_prenotazione = {
+								'main_nome':main_nome,
+								'main_note':main_note,
+								'family':family
+							};
+	console.log(dati_prenotazione);
+	var overlay = $('#confirm-overlay');
+	overlay.empty();
+	var overlay_content  = '<h1 class="martini-confirm__container__overlay__title">conferma in corso...<br>attendere prego</h1>';
+	overlay.append(overlay_content);
+	$.ajax({
+		type: 'POST',
+		url: '/confirmation',
+		/*beforeSend: function(xhr){},*/
+		cache: false,
+		data: JSON.stringify(dati_prenotazione),
+		contentType: 'application/json; charset=utf-8',
+		// crossDomain: false,
+		dataType: 'json'
+	}).done(function(data) {
+		console.log('OK; data=%o', data);
+		var overlay = $('#confirm-overlay');
+		overlay.empty();
+		var overlay_content  = '<h1 class="martini-confirm__container__overlay__title">conferma riuscita</h1>';
+		overlay.append(overlay_content);
+	}).fail(function(jqXHR, textStatus) {
+		console.log('FAILURE: %o', jqXHR);
+		console.log('FAILURE: %o', textStatus);
+	});
 }
--- src/martini.py
+++ src/martini.py
@@ -1,5 +1,11 @@
 import os
 import random
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import json
+import datetime as dt
+
 from flask import *  # lazy.
 app = Flask(__name__)
 
@@ -11,6 +17,56 @@ def set_cookie_and_redirect(request):
         res = make_response(redirect(f'http://{base_host}'))
         res.set_cookie(LOGIN_COOKIE_NAME, 'True', domain=f'{base_host}')
         return res
+
+
+
+
+class DBManager(object):
+    def __init__(self):
+        self.scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+        #self.creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+        self.creds_dict = {}#non metto la chiave di accesso in pubblico!
+        self.creds = ServiceAccountCredentials.from_json_keyfile_dict(self.creds_dict, self.scope)
+        self.client  = gspread.authorize(self.creds)
+
+        self.sheet_family = self.client.open("MartiniWedding").worksheet("famiglie")
+        self.sheet_confirmation = self.client.open("MartiniWedding").worksheet("conferme")
+
+    def get_family(self, target):
+        try:
+            selected = self.sheet_family.find(target)
+        except gspread.exceptions.CellNotFound as e:
+            print(target, " non trovato")
+            return None
+
+        selected_family = self.sheet_family.row_values(selected.row)
+        cleaned_family = [x for x in selected_family if x != target]
+        return cleaned_family
+
+    def set_confirmation(self, target, menu, notes, author, is_plusone_of):
+        try:
+            conf = self.sheet_confirmation.find(target) if is_plusone_of is None else self.sheet_confirmation.find("p1_" + is_plusone_of)
+        except gspread.exceptions.CellNotFound as e:
+            print(target, " non trovato per la conferma")
+            return False
+        # se +1 salvo anche il nome
+        if is_plusone_of is not None:
+            self.sheet_confirmation.update_cell(conf.row, conf.col, target)
+            #TODO: aggiornare anche il foglio con le famiglie?????
+            ref = self.sheet_family.find("p1_" + is_plusone_of)
+            self.sheet_family.update_cell(ref.row, ref.col, target)
+
+        self.sheet_confirmation.update_cell(conf.row, conf.col+1, menu)
+        self.sheet_confirmation.update_cell(conf.row, conf.col+2, notes)
+        self.sheet_confirmation.update_cell(conf.row, conf.col+3, author)
+        self.sheet_confirmation.update_cell(conf.row, conf.col+4, dt.datetime.now().strftime("%Y-%m-%d %H:%M"))
+        # se +1 salvo il riferimento di quello che ha dato il +1
+        if is_plusone_of is not None:
+            self.sheet_confirmation.update_cell(conf.row, conf.col+5, is_plusone_of)
+        return True
+
+
+
 
 
 @app.route('/')
@@ -35,19 +91,41 @@ def login():
 
     return render_template('login.html', static_root=STATIC_ROOT)
 
-'''
-@app.route('/get_family',methods=['POST'])
-def get_family():
-    target = json.loads(request.json['data'])["target"]
-    #use target to search db for family
-    pass
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
-@app.route('/confirmation',methods=['POST'])
+@app.route('/get_family', methods=['POST',])
+def get_family():
+    dbman = DBManager()
+    target = request.json['target']
+    family = dbman.get_family(target)
+    is_plus1 = False
+    if len(family) == 1:
+        if "p1_" in family[0]:
+            is_plus1 = True
+    return json.dumps({'success':True, 'family':family, 'target':target, 'is_plusone':is_plus1}), 200, {'ContentType':'application/json'}
+
+
+@app.route('/confirmation',methods=['POST',])
 def confirmation():
-    targets = json.loads(request.json['data'])["targets"]
-    for target in targets.keys():
+    dbman = DBManager()
+    family = request.json['family']
+    main_nome = request.json['main_nome']
+    main_note = request.json['main_note']
+    for member in family:
         #update target confirmation on db
-        pass
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
-'''
+        if 'is_plusone_of' in member.keys():
+            dbman.set_confirmation(
+                                    member['nome'],
+                                    member['menu'],
+                                    main_note if main_nome == member['nome'] else '',
+                                    main_nome,
+                                    member['is_plusone_of']
+                                    )
+        else:
+            dbman.set_confirmation(
+                                    member['nome'],
+                                    member['menu'],
+                                    main_note if main_nome == member['nome'] else '',
+                                    main_nome,
+                                    None
+                                    )
+    return json.dumps({'success':True, 'data':request.json,}), 200, {'ContentType':'application/json'} 
