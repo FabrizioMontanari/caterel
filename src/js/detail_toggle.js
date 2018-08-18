ab9ceaf99dd27a511ec08762fbd19d406654b10c
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-08-18 20:06:36 +0100

    fix detail_toggle

--- src/js/detail_toggle.js
+++ src/js/detail_toggle.js
@@ -1,17 +1,17 @@
-const elementIsVisible = el => el && el.style.maxHeight//el && el.style && el.style.display !== 'none'; 
+const elementIsVisible = function (el) { return el && el.style.maxHeight; }
 
-const hideElement = el => el.style.maxHeight=null;//el.style.display = 'none';
+const hideElement = function (el) { el.style.maxHeight = null; }
 
-const showElement = el => el.style.maxHeight = el.scrollHeight + "px";//el.style.removeProperty('display');
+const showElement = function (el) { el.style.maxHeight = el.scrollHeight + "px"; }
 
-const toggleVisibility = el => elementIsVisible(el) ? hideElement(el) : showElement(el);
+const toggleVisibility = function (el) { elementIsVisible(el) ? hideElement(el) : showElement(el); }
 
-const initialise_gift_details = () => {
+const initGiftToggle = function () {
     const giftDetails = document.querySelector('.martini-gift__gift-details');
     const toggleLink = document.querySelector('.martini-gift__toggle-details');
 
     hideElement(giftDetails);
-    toggleLink.addEventListener('click', () => toggleVisibility(giftDetails));
+    toggleLink.addEventListener('click', function () { toggleVisibility(giftDetails) });
 };
 
-document.addEventListener('DOMContentLoaded', initialise_gift_details);
+document.addEventListener('DOMContentLoaded', initGiftToggle);
