6d08c4e230938b53737bc0a8a41ec1803f60db28
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-08-18 20:03:14 +0100

    fix countdown for es5 browsers

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -1,33 +1,30 @@
-const countDownDate = new Date("Sep 22, 2018 12:00:00").getTime();
+var countDownDate = new Date("Sep 22, 2018 12:00:00").getTime();
 
-const getClassName = name => `.martini-countdown__banner-element__central__clock${name}`;
+var getClassName = function (name) { return '.martini-countdown__banner-element__central__clock' + name; }
 
-const updateCountdown = counters => {
-	const distance = countDownDate - new Date().getTime();
+var updateCountdown = function (counters) {
+	var distance = countDownDate - new Date().getTime();
 
 	if (distance < 0) {
 		return counters.clock.innerHTML = 'Si sono sposati!';
 	}
 
-	const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-	const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	/*const seconds = Math.floor((distance % (1000 * 60)) / 1000);*/
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 
-	counters.days.innerHTML = `${days}D&nbsp;`;
-	counters.hours.innerHTML = `&nbsp;${hours}H&nbsp;`;
-	counters.minutes.innerHTML = `&nbsp;${minutes}M`;
-	/*counters.seconds.innerHTML = `${seconds}S`;*/
+	counters.days.innerHTML = days + 'D&nbsp;';
+	counters.hours.innerHTML = '&nbsp;' + hours + 'H&nbsp;';
+	counters.minutes.innerHTML = '&nbsp;' + minutes + 'M';
 
-	setTimeout(() => updateCountdown(counters), 60000);
+	setTimeout(function () { updateCountdown(counters) }, 60000);
 };
 
-const initialise = () => {
-	const counterElements = {
+var initialise = function () {
+	var counterElements = {
 		days: document.querySelector(getClassName('-days')),
 		hours: document.querySelector(getClassName('-hours')),
 		minutes: document.querySelector(getClassName('-minutes')),
-		/*seconds: document.querySelector(getClassName('-seconds')),*/
 		clock: document.querySelector(getClassName(''))
 	}
 
